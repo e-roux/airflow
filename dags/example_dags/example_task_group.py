c32bfe9fcf26d4c1dba2b00c8d1b3f2e93b9c84a
Emmanuel Roux <fesaille@users.noreply.github.com>
2021-04-15 18:32:39 +0200

    chore: add testing and executor parts

--- dags/example_dags/example_task_group.py
+++ dags/example_dags/example_task_group.py
@@ -18,18 +18,27 @@
 
 """Example DAG demonstrating the usage of the TaskGroup."""
 
+import random
+
 from airflow.models.dag import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
+from airflow.utils.helpers import chain
 from airflow.utils.task_group import TaskGroup
+
+NOPS = random.choice((5, 6, 7, 8))
 
 with DAG(dag_id="example_task_group", start_date=days_ago(2), tags=["example"]) as dag:
     start = DummyOperator(task_id="start")
 
+    with TaskGroup("section_0", tooltip="Tasks for section_01") as section_0:
+
+        chain([DummyOperator(task_id=f"task_{i}") for i in range(NOPS)])
+
     with TaskGroup("section_1", tooltip="Tasks for section_1") as section_1:
         task_1 = DummyOperator(task_id="task_1")
-        task_2 = BashOperator(task_id="task_2", bash_command='echo 1')
+        task_2 = BashOperator(task_id="task_2", bash_command="echo 1")
         task_3 = DummyOperator(task_id="task_3")
 
         task_1 >> [task_2, task_3]
@@ -37,14 +46,15 @@ with DAG(dag_id="example_task_group", start_date=days_ago(2), tags=["example"]) 
     with TaskGroup("section_2", tooltip="Tasks for section_2") as section_2:
         task_1 = DummyOperator(task_id="task_1")
 
-        with TaskGroup("inner_section_2", tooltip="Tasks for inner_section2") as inner_section_2:
-            task_2 = BashOperator(task_id="task_2", bash_command='echo 1')
+        with TaskGroup(
+            "inner_section_2", tooltip="Tasks for inner_section2"
+        ) as inner_section_2:
+            task_2 = BashOperator(task_id="task_2", bash_command="echo 1")
             task_3 = DummyOperator(task_id="task_3")
             task_4 = DummyOperator(task_id="task_4")
 
             [task_2, task_3] >> task_4
 
+    end = DummyOperator(task_id="end")
 
-    end = DummyOperator(task_id='end')
-
-    start >> section_1 >> section_2 >> end
+    start >> section_0 >> section_1 >> section_2 >> end
